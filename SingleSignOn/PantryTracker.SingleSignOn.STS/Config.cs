a31a06c633380f16df2c7e71ba6ee804c2d57ce6
Jeremy McQuivey <jeremymcquivey@gmail.com>
2019-09-12 15:51:28 -0500

    Dev introduce central signon (#10)
    * introduce central signon server

---
+++ SingleSignOn/PantryTracker.SingleSignOn.STS/Config.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace PantryTracker.SingleSignOn.STS{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("projects-api", "Projects API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "pantrytracker-recipe-client",
+                    ClientName = "PantryTracker Recipes",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+                    RequireConsent = false,
+
+                    RedirectUris =           { "http://localhost:4200/assets/oidc-login-redirect.html" },
+                    PostLogoutRedirectUris = { "http://localhost:4200/?postLogout=true" },
+                    AllowedCorsOrigins =     { "http://localhost:4200/" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "projects-api"
+                    }
+                }
+            };
+
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+    }
+}
