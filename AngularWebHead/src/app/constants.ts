bbd33356c7ce259d883ca41ccd9220e9a6078bd2
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2019-09-28 08:46:17 -0500

    deployed recipe api to cloud. use hosted instance instead of local.

--- AngularWebHead/src/app/constants.ts
+++ AngularWebHead/src/app/constants.ts
@@ -1,9 +1,11 @@
 export class Constants {
   public static apiRoot = 'http://localhost:2112/api/';
-  public static recipeApi = 'http://localhost:64325/api/';
-  public static stsAuthority = 'https://pantrytrackers-identity-dev.azurewebsites.net/';
+  //public static recipeApi = 'http://localhost:64325/api/';
+  public static recipeApi = 'https://pantrytrackers-recipeapi-dev.azurewebsites.net/api/';
+  public static stsAuthority = 'https://pantrytracker-identity-dev.azurewebsites.net/';
 
   public static clientId = 'pantrytrackers-ui';
-
-  public static clientRoot = 'http://localhost:4200/';
+  public static clientRoot = window.location.hostname === 'localhost' ? 
+                                'http://' + window.location.hostname + ':' + window.location.port + '/' :
+                                'https://' + window.location.hostname + '/';
 }
