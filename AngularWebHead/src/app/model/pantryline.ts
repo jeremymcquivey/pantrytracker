789cf8f1e72a9b807724a4ade34c8d5ae4f2b55d
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-01-28 17:51:32 -0600

    enable ui to query current pantry inventory

---
+++ AngularWebHead/src/app/model/pantryline.ts
@@ -0,0 +1,13 @@
+export class PantryLine {
+    public quantity: string;
+
+    public product: Product;
+
+    public unit: string;
+}
+
+export class Product {
+    public name: string;
+
+    public id: number;
+}
