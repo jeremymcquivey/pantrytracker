1c4cbe13245f9a803720714842f4f872c2457df0
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-01-30 23:26:56 -0600

    make product optional on product code.

---
+++ RecipeAPI/RecipeAPI/Migrations/20200131051401_productCodeOptionalProduct.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeAPI.Migrations
+{
+    public partial class productCodeOptionalProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductCodes_Products_ProductId",
+                table: "ProductCodes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "ProductCodes",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductCodes_Products_ProductId",
+                table: "ProductCodes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductCodes_Products_ProductId",
+                table: "ProductCodes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "ProductCodes",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductCodes_Products_ProductId",
+                table: "ProductCodes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
