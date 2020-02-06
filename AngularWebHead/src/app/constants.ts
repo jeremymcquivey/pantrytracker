d126d5703e70932903fa2efb2153a2ab57797ec1
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-02-05 21:04:06 -0600

    reset constants file on angular app

--- AngularWebHead/src/app/constants.ts
+++ AngularWebHead/src/app/constants.ts
@@ -1,8 +1,8 @@
 export class Constants {
   public static apiRoot = 'http://localhost:2112/api/';
-  //public static recipeApi = 'http://localhost:64325/api/';
+  //public static recipeApi = 'http://localhost:64326/api/';
   public static recipeApi = 'https://pantrytrackers-recipeapi-dev.azurewebsites.net/api/';
-  public static stsAuthority = 'https://pantrytracker-identity-dev.azurewebsites.net/';
+  public static stsAuthority = 'https://pantrytrackers-identity-dev.azurewebsites.net/';
 
   public static clientId = 'pantrytrackers-ui';
   public static clientRoot = window.location.hostname === 'localhost' ? 
