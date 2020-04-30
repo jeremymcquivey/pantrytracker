7e7155ef7316c20d8874c2ce83f7eefc31a2eb47
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-04-29 21:36:07 -0500

    new migration for allowing user preferences to include varieties.

---
+++ RecipeAPI/RecipeAPI/Migrations/20200430000401_trackProductPreferenceWithVariety.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeAPI.Migrations
+{
+    public partial class trackProductPreferenceWithVariety : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VarietyId",
+                table: "UserProductPreferences",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProductPreferences_VarietyId",
+                table: "UserProductPreferences",
+                column: "VarietyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_VarietyId",
+                table: "Transactions",
+                column: "VarietyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Varieties_VarietyId",
+                table: "Transactions",
+                column: "VarietyId",
+                principalTable: "Varieties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserProductPreferences_Varieties_VarietyId",
+                table: "UserProductPreferences",
+                column: "VarietyId",
+                principalTable: "Varieties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Varieties_VarietyId",
+                table: "Transactions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserProductPreferences_Varieties_VarietyId",
+                table: "UserProductPreferences");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserProductPreferences_VarietyId",
+                table: "UserProductPreferences");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_VarietyId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "VarietyId",
+                table: "UserProductPreferences");
+        }
+    }
+}
