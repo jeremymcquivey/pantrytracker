e8a455086972648b116ad281a14c107b327a5ae7
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-04-30 23:32:16 -0500

    updated some units for conversion.

--- RecipeAPI/Model/PantryTracker.Model.Recipe/Products/UnitConversionRate.cs
+++ RecipeAPI/Model/PantryTracker.Model.Recipe/Products/UnitConversionRate.cs
@@ -2,12 +2,12 @@
 {
     public class UnitConversionRate
     {
-        public string PrimaryUnit { get; set; }
+        public string SourceUnit { get; set; }
 
-        public string SecondaryUnit { get; set; }
+        public string DestinationUnit { get; set; }
 
         public double ConversionScale { get; set; }
 
-        public int? ProductId { get; set; }
+        public int[] Products { get; set; }
     }
 }
---
+++ RecipeAPI/RecipeAPI/Models/Units.cs
@@ -0,0 +1,33 @@
+﻿namespace RecipeAPI.Models
+{
+    public class UnitsOfWeight
+    {
+        public const string Milligram = "mg";
+        public const string Ounce = "oz";
+        public const string Gram = "g";
+        public const string Pound = "lb";
+        public const string Kilogram = "kg";
+    }
+
+    public class UnitsOfVolume
+    {
+        public const string Teaspoon = "tsp";
+        public const string Tablespoon = "tbsp";
+        public const string Cup = "c";
+        public const string Pint = "pt";
+        public const string Quart = "qt";
+        public const string Gallon = "gal";
+        public const string FluidOunce = "fl oz";
+        public const string Milliliter = "ml";
+        public const string Liter = "l";
+    }
+
+    public class OtherUnits
+    {
+        public const string Pinch = "pinch";
+        public const string Dash = "dash";
+        public const string Jigger = "jigger";
+        public const string Cube = "cube";
+        public const string Square = "sq";
+    }
+}
