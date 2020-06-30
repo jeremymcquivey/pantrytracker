92060181e9ba791060ca03ec81430170dec668f0
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-06-30 03:39:53 -0500

    pull both purchased and active items when retrieving grocery list.

--- RecipeAPI/RecipeAPI/Controllers/ShoppingListController.cs
+++ RecipeAPI/RecipeAPI/Controllers/ShoppingListController.cs
@@ -54,7 +54,7 @@ namespace RecipeAPI.Controllers
             }
 
             //TODO: Combine by product and unit.
-            return Ok(_database.GroceryListItems.Where(item => item.PantryId == id && item.Status == (ListItemStatus.Active | ListItemStatus.Purchased))
+            return Ok(_database.GroceryListItems.Where(item => item.PantryId == id && new[] { ListItemStatus.Active, ListItemStatus.Purchased }.Contains(item.Status))
                                                 .Include(item => item.Variety)
                                                 .Include(item => item.Product)
                                                 .ToList());
