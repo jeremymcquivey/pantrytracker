62d27dc6f924e9a6c164346cb3e85eb997287278
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-06-29 20:48:17 -0500

    missed a mobile file.

---
+++ Mobile/PantryTrackers/PantryTrackers/ViewModels/UnauthorizedViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Navigation;
+
+namespace PantryTrackers.ViewModels
+{
+    public class UnauthorizedViewModel: ViewModelBase
+    {
+        public UnauthorizedViewModel(INavigationService navService)
+            :base(navService)
+        {
+
+        }
+    }
+}
