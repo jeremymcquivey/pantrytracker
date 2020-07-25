8a08ea4914f888c3d58080f8b40ec7de380cac72
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-07-25 02:13:31 -0500

    add properties to grocery list to allow user to edit quantity and container size of items being sent to the list from a recipe.

---
+++ RecipeAPI/RecipeAPI/Migrations/20200725051140_addQuantityAndUnitToListPreference.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeAPI.Migrations
+{
+    public partial class addQuantityAndUnitToListPreference : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Quantity",
+                table: "UserProductPreferences",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "UserProductPreferences",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "UserProductPreferences",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "UserProductPreferences");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "UserProductPreferences");
+
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "UserProductPreferences");
+        }
+    }
+}
