2ee8c6925ca32805e9c3933eaceb25c1492d888d
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-07-25 02:17:48 -0500

    container things -- still very much in progress.

---
+++ RecipeAPI/Data Import/PantryTracker.RecipeReader/Rules/WordRules/DetectContainer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PantryTracker.RecipeReader.Rules
+{
+    internal static partial class EnumerableRules
+    {
+        public static IEnumerable<Word> DetectContainer(this IEnumerable<Word> wordList)
+        {
+            return wordList;
+        }
+    }
+}
---
+++ RecipeAPI/Model/PantryTracker.Model.Recipe/Meta/Containers.cs
@@ -0,0 +1,10 @@
+﻿namespace PantryTracker.Model.Meta
+{
+    class Containers
+    {
+        public const string Jar = "jar";
+        public const string Case = "case";
+        public const string Bag = "bag";
+        public const string Bucket = "bucket";
+    }
+}
