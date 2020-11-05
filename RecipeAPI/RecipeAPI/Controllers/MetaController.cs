b690abdac2d2f1f090c8612eebe482f09ae0a2ca
Jeremy McQuivey <jeremymcquivey@hotmail.com>
2020-11-05 16:27:43 -0600

    api updates for mobile app

---
+++ RecipeAPI/RecipeAPI/Controllers/MetaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MetaController : BaseController
+    {
+        [HttpGet]
+        public IActionResult GetMetaData()
+        {
+            return Ok(new 
+            { 
+                ApiVersion = "1.0.0",
+                Android = new 
+                {
+                    MinimumAppVersion = "1",
+                    MinimumAppVersionName = "1.0"
+                },
+                iOS = new
+                {
+                    MinimumAppVersion = "1.0",
+                    MinimumAppVersionName = "1.0"
+                },
+                STSUrl = "",
+            });
+        }
+    }
+}
