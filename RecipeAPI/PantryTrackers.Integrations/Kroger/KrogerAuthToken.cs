3e27021a1edb9c5b185093d248f516645647992f
Jeremy McQuivey <jeremymcquivey@gmail.com>
2021-01-23 16:08:23 -0600

    update to new walmart api

--- RecipeAPI/PantryTrackers.Integrations/Kroger/KrogerAuthToken.cs
+++ RecipeAPI/PantryTrackers.Integrations/Kroger/KrogerAuthToken.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PantryTrackers.Integrations.Kroger
+﻿namespace PantryTrackers.Integrations.Kroger
 {
     public class KrogerAuthToken
     {
---
+++ RecipeAPI/PantryTrackers.Integrations/Walmart/WalmartAuthToken.cs
@@ -0,0 +1,13 @@
+﻿namespace PantryTrackers.Integrations.Walmart
+{
+    public class WalmartAuthToken
+    {
+        public string Signature { get; set; }
+
+        public long TimeStamp { get; set; }
+
+        public long ValidUntil { get; set; }
+
+        public string ConsumerId { get; set; }
+    }
+}
