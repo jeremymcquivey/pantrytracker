4b7a65d44fa7d0620036d8260fd41258321af284
Jeremy McQuivey <jeremymcquivey@gmail.com>
2021-02-16 17:14:51 -0600

    introduce push notifications

--- Mobile/PantryTrackers/PantryTrackers.Android/MainActivity.cs
+++ Mobile/PantryTrackers/PantryTrackers.Android/MainActivity.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Gms.Common;
 using Android.OS;
+using Android.Util;
 using PantryTrackers.Config;
+using PantryTrackers.Droid.Notifications;
 using Prism;
 using Prism.Ioc;
 
@@ -20,12 +23,52 @@ namespace PantryTrackers.Droid
 
             base.OnCreate(bundle);
 
+            IsPlayServicesAvailable();
+            CreateNotificationChannel();
+
             // 3rd party initializations
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
 
             Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
             Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, bundle);
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            var tag = nameof(MainActivity);
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(tag, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(tag, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+
+            Log.Debug(tag, "Google Play Services is available.");
+            return true;
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                return;
+            }
+
+
+            var channel = new NotificationChannel(NotificationConstants.ChannelId, NotificationConstants.ChannelName, NotificationImportance.Default)
+            {
+                Description = string.Empty
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 
