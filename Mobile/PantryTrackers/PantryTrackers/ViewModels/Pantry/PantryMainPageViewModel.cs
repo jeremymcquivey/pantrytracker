01bab3a3b01c299e144e0aaa81c2dcb3b6ea9bfd
Jeremy McQuivey <jeremymcquivey@gmail.com>
2021-05-16 02:18:54 -0500

    add context menu onto patry list

--- Mobile/PantryTrackers/PantryTrackers/ViewModels/Pantry/PantryMainPageViewModel.cs
+++ Mobile/PantryTrackers/PantryTrackers/ViewModels/Pantry/PantryMainPageViewModel.cs
@@ -1,15 +1,10 @@
-﻿using PantryTrackers.Common.Extensions;
 using PantryTrackers.Common.Security;
 using PantryTrackers.Models;
 using PantryTrackers.Models.Meta.Enums;
 using PantryTrackers.Services;
 using PantryTrackers.Views.Pantry;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -19,16 +14,43 @@ namespace PantryTrackers.ViewModels.Pantry
     {
         private Command _addInventoryCommand;
         private Command _removeInventoryCommand;
+        private Command _addItemToListCommand;
+        private Command _someCommand;
+
         private readonly PantryService _pantry;
         private readonly INavigationService _navigationService;
 
-        public Command SomeCommand => new Command(() =>
+        public Command SomeCommand => _someCommand ??= new Command(async () =>
         {
-            //ProductGroups = new List<ProductGroup>();
-        });
+            // TODO: Eventually, when we support multiple pantries, this will be the pantry requested.
+            // For now, though, this is the user Id.
+            var pantryId = await SecureStorage.GetAsync(ClaimKeys.Id);
+
+            await Device.InvokeOnMainThreadAsync(() =>
+            {
+                IsNetworkBusy = true;
+            });
+
+            var data = await _pantry.GetSummary(pantryId);
+
+            await Device.InvokeOnMainThreadAsync(() =>
+            {
+                ProductGroups.Clear();
+                foreach (var group in data)
+                {
+                    ProductGroups.Add(group);
+                }
+                IsNetworkBusy = false;
+            });
+        }, CanExecute);
 
         public ObservableCollection<ProductGroup> ProductGroups { get; } =
             new ObservableCollection<ProductGroup>();
+
+        public Command AddItemToListCommand => _addItemToListCommand ??= new Command((myParam) =>
+        {
+            SomeCommand.Execute(null);
+        });
 
         public Command AddInventoryCommand => _addInventoryCommand ??= new Command(async () =>
         {
@@ -59,27 +81,7 @@ namespace PantryTrackers.ViewModels.Pantry
         public override async void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
-
-            // TODO: Eventually, when we support multiple pantries, this will be the pantry requested.
-            // For now, though, this is the user Id.
-            var pantryId = await SecureStorage.GetAsync(ClaimKeys.Id);
-
-            await Device.InvokeOnMainThreadAsync(() =>
-            {
-                IsNetworkBusy = true;
-            });
-
-            var data = await _pantry.GetSummary(pantryId);
-
-            await Device.InvokeOnMainThreadAsync(() =>
-            {
-                ProductGroups.Clear();
-                foreach (var group in data)
-                {
-                    ProductGroups.Add(group);
-                }
-                IsNetworkBusy = false;
-            });
+            SomeCommand.Execute(null);
         }
 
         public override void OnCommandCanExecuteChanged()
@@ -88,6 +90,8 @@ namespace PantryTrackers.ViewModels.Pantry
 
             AddInventoryCommand.ChangeCanExecute();
             RemoveInventoryCommand.ChangeCanExecute();
+            SomeCommand.ChangeCanExecute();
+            AddItemToListCommand.ChangeCanExecute();
         }
     }
 }
